64587e452915b8d56376b454b8aec25dbe285431
Chris Mills <crmills@fsu.edu>
2024-11-10 16:26:40 -0500

    Optimize screen size on app launch

--- SuperTicTacToe/App.xaml.cs
+++ SuperTicTacToe/App.xaml.cs
@@ -8,5 +8,18 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            // Set the window to fill the available screen space.
+            window.MaximumWidth = DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density;
+            window.MaximumHeight = DeviceDisplay.MainDisplayInfo.Height / DeviceDisplay.MainDisplayInfo.Density;
+            window.X = 0;
+            window.Y = 0;
+
+            return window;
+        }
     }
 }
